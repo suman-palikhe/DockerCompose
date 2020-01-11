17fcde3d89a768adb4cd5a734dd3fb53683a174d
Suman Palikhe <palikhe.suman@gmail.com>
2020-01-11 18:01:23 +1100

    add console app

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Hello World! Second time");
+               
+            }
+            
+        }
+    }
+}
